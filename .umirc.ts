04393dd4018debbd201943180f345542c43045bd
v-wangde <v-wangde@xiaomi.com>
2025-12-02 16:59:04 +0800

    feat:ccc

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
-  ],
   npmClient: 'pnpm',
   base: '/DAY-Mobile/',
   publicPath: '/DAY-Mobile/',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,50 @@
-import yayJpg from '../assets/yay.jpg';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+
+import img1 from '../assets/20251202-165356.jpeg';
+import img2 from '../assets/20251202-165406.jpeg';
+import img3 from '../assets/20251202-165413.jpeg';
+
+import styles from './index.less';
 
 export default function HomePage() {
+  const slides = [
+    { id: 2, image: img1, title: 'Image 1' },
+    { id: 3, image: img2, title: 'Image 2' },
+    { id: 4, image: img3, title: 'Image 3' },
+  ];
+
   return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
+    <div className={styles.homeContainer}>
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        className={styles.swiper}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <div className={styles.slideContent}>
+              <img src={slide.image} alt={slide.title} className={styles.slideImage} />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      
+      <div className={styles.welcomeSection}>
+        <h2 className={styles.welcomeTitle}>Yay! Welcome to Mi Mobile!</h2>
+        <p className={styles.description}>
+          Enjoy the beautiful carousel experience on your device.
+        </p>
+      </div>
     </div>
   );
 }
